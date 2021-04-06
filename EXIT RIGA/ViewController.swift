3aacd70b0f4bef3192f2cd7cdfc8b6615d786b2f
vishal singh <er.vishalsingh16@gmail.com>
2021-04-06 10:08:37 +0530

    Initial Commit

---
+++ EXIT RIGA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EXIT RIGA
+//
+//  Created by vishal_singh on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
