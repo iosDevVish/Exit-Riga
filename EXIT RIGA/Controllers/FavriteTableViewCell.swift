1f048783466e3a9820b3e8abd2c5a195c2435a43
vishal singh <er.vishalsingh16@gmail.com>
2021-04-13 16:29:00 +0530

    All Integration

---
+++ EXIT RIGA/Controllers/FavriteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavriteTableViewCell.swift
+//  EXIT RIGA
+//
+//  Created by vishal_singh on 09/04/21.
+//
+
+import UIKit
+
+class FavriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
